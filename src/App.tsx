206f60d2ccaa302212bac5519d7f786d9f921930
marcosbaez1 <marcosbaez.it@gmail.com>
2026-06-29 12:31:27 -0300

    feat: scaffold panel admin con React + Vite + MUI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Typography, Button } from '@mui/material'
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Panel Admin
+      </Typography>
+      <Button variant="contained">Botón de prueba</Button>
+    </Container>
+  )
+}
+
+export default App
